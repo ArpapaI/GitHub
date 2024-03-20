bee5129149698123bc29438394b4a34b8e548c5e
ArpapaI <pavluckovivan243@gmail.com>
2024-03-20 21:07:45 +0300

    Страдали херней

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
 int main(void)
 {
+	system("chcp 1251");
+	system("cls");
+	int a;
+	int* p;
+	p = &a;
+	scanf("%d", p);
+	printf("%d\n", a);
 	return 0;
 }
